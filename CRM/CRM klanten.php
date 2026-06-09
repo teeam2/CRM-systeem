87b60eefd4a1813210d62e2166599048baf50b28
finn bloemers <finn.bloemers@student.gildeopleidingen.nl>
2026-06-09 11:24:04 +0200

    database gelinkt
    voor klanten, medewerkers en opdrachten

--- CRM/CRM klanten.php
+++ CRM/CRM klanten.php
@@ -28,7 +28,7 @@ ini_set('display_errors', 1);
      $db_server = "localhost";
      $db_user = "root";
      $db_pass = "";
-     $db_name = "crm";
+     $db_name = "crm_systeem";
      $conn = "";
 
      $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
