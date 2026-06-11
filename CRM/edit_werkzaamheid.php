4ecdac81a2a10f08b0b4394ef07a21841deb93b4
finn bloemers <finn.bloemers@student.gildeopleidingen.nl>
2026-06-11 13:21:40 +0200

    add, edit, delete toevoegen
    ik heb bij werkzaamheden
    add, edit, delete
    toegevoegd

---
+++ CRM/edit_werkzaamheid.php
@@ -0,0 +1,48 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "crm_systeem");
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM werkzaamheden WHERE werkzaamheid_id=$id";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if(isset($_POST['opslaan']))
+{
+    $sql = "UPDATE werkzaamheden SET
+    medewerker_id='$_POST[medewerker_id]',
+    opdracht_id='$_POST[opdracht_id]',
+    datum='$_POST[datum]',
+    aantal_uren='$_POST[aantal_uren]',
+    omschrijving='$_POST[omschrijving]'
+    WHERE werkzaamheid_id=$id";
+
+    mysqli_query($conn, $sql);
+
+    header("Location: CRM werkzaamheden.php");
+    exit();
+}
+?>
+
+<h2>Werkzaamheid wijzigen</h2>
+
+<form method="post">
+
+Medewerker ID:<br>
+<input type="text" name="medewerker_id" value="<?= $row['medewerker_id'] ?>"><br><br>
+
+Opdracht ID:<br>
+<input type="text" name="opdracht_id" value="<?= $row['opdracht_id'] ?>"><br><br>
+
+Datum:<br>
+<input type="date" name="datum" value="<?= $row['datum'] ?>"><br><br>
+
+Aantal uren:<br>
+<input type="number" step="0.1" name="aantal_uren" value="<?= $row['aantal_uren'] ?>"><br><br>
+
+Omschrijving:<br>
+<textarea name="omschrijving"><?= $row['omschrijving'] ?></textarea><br><br>
+
+<input type="submit" name="opslaan" value="Opslaan">
+
+</form>
