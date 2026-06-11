369a4464ab7c4a569f1399364d3c9d3e8327614b
YinZoo <ramisowalha3@gmail.com>
2026-06-11 13:24:39 +0200

    Medewerker rol permissions gelimiteerd
    - Toegang wordt gewijgerd uit alle pages buiten :
    
    * Home
    * Werkzaamheden
    * Inloggen

--- CRM/CRM opdrachten.php
+++ CRM/CRM opdrachten.php
@@ -5,6 +5,10 @@ if(!isset($_SESSION['voornaam'])){
     die("Je moet eerst inloggen");
 }
 
+if($_SESSION['rol'] != 'admin'){
+    die("Geen toegang");
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
