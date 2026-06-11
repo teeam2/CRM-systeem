b5c3fb8f96f6264c1571eee6f60604ef4e689821
finn bloemers <finn.bloemers@student.gildeopleidingen.nl>
2026-06-11 11:44:20 +0200

    add, edit, delete toevoegen
    ik heb bij facturen
    add, edit, delete
    toegevoegd

---
+++ CRM/add_factuur.php
@@ -0,0 +1,45 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "crm_systeem");
+
+if(isset($_POST['toevoegen']))
+{
+    $klant_id = $_POST['klant_id'];
+    $opdracht_id = $_POST['opdracht_id'];
+    $factuurdatum = $_POST['factuurdatum'];
+    $totaalbedrag = $_POST['totaalbedrag'];
+    $status = $_POST['status'];
+
+    $sql = "INSERT INTO facturen
+    (klant_id, opdracht_id, factuurdatum, totaalbedrag, status)
+    VALUES
+    ('$klant_id', '$opdracht_id', '$factuurdatum', '$totaalbedrag', '$status')";
+
+    mysqli_query($conn, $sql);
+
+    header("Location: CRM facturen.php");
+    exit();
+}
+?>
+
+<h2>Nieuwe factuur</h2>
+
+<form method="post">
+
+Klant ID:<br>
+<input type="text" name="klant_id"><br><br>
+
+Opdracht ID:<br>
+<input type="text" name="opdracht_id"><br><br>
+
+Factuurdatum:<br>
+<input type="date" name="factuurdatum"><br><br>
+
+Totaalbedrag:<br>
+<input type="text" name="totaalbedrag"><br><br>
+
+Status:<br>
+<input type="text" name="status"><br><br>
+
+<input type="submit" name="toevoegen" value="Toevoegen">
+
+</form>
