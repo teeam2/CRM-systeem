28fe5ae88afbc347129b22cf278ec056e1dbc752
finn bloemers <finn.bloemers@student.gildeopleidingen.nl>
2026-06-30 10:30:02 +0200

    facturen toevoegen updaten

--- CRM/CRM facturen/add_factuur.php
+++ CRM/CRM facturen/add_factuur.php
@@ -14,6 +14,18 @@ ini_set('display_errors', 1);
 
 $conn = mysqli_connect("localhost", "root", "", "crm_systeem");
 
+$klanten = mysqli_query($conn, "
+    SELECT klanten_ID, Voornaam 
+    FROM klanten 
+    ORDER BY klanten_ID
+");
+
+$opdrachten = mysqli_query($conn, "
+    SELECT opdracht_id, titel 
+    FROM opdrachten 
+    ORDER BY opdracht_id
+");
+
 if(isset($_POST['toevoegen']))
 {
     $klant_id = $_POST['klant_id'];
@@ -40,20 +52,47 @@ if(isset($_POST['toevoegen']))
 
 <form method="post">
 
-Klant ID:<br>
-<input type="text" name="klant_id"><br><br>
+Klant:<br>
 
-Opdracht ID:<br>
-<input type="text" name="opdracht_id"><br><br>
+<select name="klant_id" required>
+
+    <?php while($k = mysqli_fetch_assoc($klanten)) { ?>
+        <option value="<?php echo $k['klanten_ID']; ?>">
+            <?php echo $k['klanten_ID'] . " - " . $k['Voornaam']; ?>
+        </option>
+    <?php } ?>
+
+</select>
+
+<br><br>
+
+Opdracht:<br>
+
+<select name="opdracht_id" required>
+
+    <?php while($o = mysqli_fetch_assoc($opdrachten)) { ?>
+        <option value="<?php echo $o['opdracht_id']; ?>">
+            <?php echo $o['opdracht_id'] . " - " . $o['titel']; ?>
+        </option>
+    <?php } ?>
+
+</select>
+
+<br><br>
 
 Factuurdatum:<br>
 <input type="date" name="factuurdatum"><br><br>
 
 Totaalbedrag:<br>
-<input type="text" name="totaalbedrag"><br><br>
+<input type="number" name="totaalbedrag" min="0" step="0.01" required><br><br>
 
-Status:<br>
-<input type="text" name="status"><br><br>
+
+Status: <select id="status" name="status"> 
+    <option value="betaald">betaald</option> 
+    <option value="verzonden">verzonden</option>
+    <option value="open">open</option>
+    <option value="te_laat">te_laat</option>
+</select><br>
 
 <input type="submit" name="toevoegen" value="Toevoegen">
 
