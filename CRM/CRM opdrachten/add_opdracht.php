cb866512bd57c3ee7d62ea9c3ead276a10a421ef
finn bloemers <finn.bloemers@student.gildeopleidingen.nl>
2026-06-30 09:55:29 +0200

    opdracht toevoegen updaten

--- CRM/CRM opdrachten/add_opdracht.php
+++ CRM/CRM opdrachten/add_opdracht.php
@@ -13,6 +13,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $conn = mysqli_connect("localhost", "root", "", "crm_systeem");
 
+$klanten = mysqli_query($conn, "SELECT klanten_ID, Voornaam FROM klanten ORDER BY klanten_ID");
+
 if(isset($_POST['toevoegen']))
 {
     $klant_id = $_POST['klant_id'];
@@ -41,8 +43,16 @@ if(isset($_POST['toevoegen']))
 
 <form method="post">
 
-Klant ID:<br>
-<input type="text" name="klant_id"><br><br>
+Klant:<br>
+<select name="klant_id" required>
+
+    <?php while($klant = mysqli_fetch_assoc($klanten)) { ?>
+        <option value="<?php echo $klant['klanten_ID']; ?>">
+            <?php echo $klant['klanten_ID'] . " - " . $klant['Voornaam']; ?>
+        </option>
+    <?php } ?>
+
+</select><br><br>
 
 Titel:<br>
 <input type="text" name="titel"><br><br>
@@ -50,11 +60,15 @@ Titel:<br>
 Beschrijving:<br>
 <textarea name="beschrijving"></textarea><br><br>
 
-Status:<br>
-<input type="text" name="status"><br><br>
+Status: <select id="status" name="status"> 
+    <option value="actief">actief</option> 
+    <option value="afgerond">afgerond</option>
+    <option value="betaald">betaald</option>
+    <option value="gefactureerd">gefactureerd</option>
+</select><br>
 
 Uurprijs:<br>
-<input type="text" name="uurprijs"><br><br>
+<input type="number" name="uurprijs" min="0" step="0.01" required><br><br>
 
 Startdatum:<br>
 <input type="date" name="startdatum"><br><br>
